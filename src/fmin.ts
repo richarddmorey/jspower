fc930b65e2c373cc2bd80001123de6df5a3ea84c
Richard Morey <richarddmorey@gmail.com>
2020-10-03 19:12:34 +0100

    convert fmin to typescript

--- src/fmin.ts
+++ src/fmin.ts
@@ -1,66 +1,95 @@
 /********
 * This is adapted from:
 * https://gist.github.com/awsnap/0dfed02ee15657df05aa
-* from user https://github.com/awsnap
+* from user https://github.com/awsnap 
 * It is almost line-for-line R's optimize() function in c
 * See https://github.com/wch/r-source/blob/trunk/src/library/stats/src/optimize.c
 *********/
-;
-function fmin(ax, bx, f, tol) {
-    "use strict";
+
+interface fmin_result {
+  x: number,
+  fx: number,
+  i: number
+};
+
+function fmin(ax: number, bx: number, f: Function, tol: number): fmin_result {
+    "use strict";    
     tol = tol || 1E-8;
-    //var ax,bx,f,tol;
-    //
-    //     an approximation  x  to the point where  f  attains a minimum  on
-    // the interval  (ax,bx)  is determined.
-    //
-    //
-    // input..
-    //
-    // ax    left endpoint of initial interval
-    // bx    right endpoint of initial interval
-    // f     function subprogram which evaluates  f(x)  for any  x
-    //       in the interval  (ax,bx)
-    // tol   desired length of the interval of uncertainty of the final
-    //       result ( >= 0.0)
-    //
-    //
-    // output..
-    //
-    // fmin  abcissa approximating the point where  f  attains a minimum
-    //
-    //
-    //     the method used is a combination of  golden  section  search  and
-    // successive parabolic interpolation.  convergence is never much slower
-    // than  that  for  a  fibonacci search.  if  f  has a continuous second
-    // derivative which is positive at the minimum (which is not  at  ax  or
-    // bx),  then  convergence  is  superlinear, and usually of the order of
-    // about  1.324....
-    //     the function  f  is never evaluated at two points closer together
-    // than  eps*Math.abs(fmin) + (tol/3), where eps is  approximately the square
-    // root  of  the  relative  machine  precision.   if   f   is a unimodal
-    // function and the computed values of   f   are  always  unimodal  when
-    // separated by at least  eps*Math.abs(x) + (tol/3), then  fmin  approximates
-    // the abcissa of the global minimum of  f  on the interval  ax,bx  with
-    // an error less than  3*eps*Math.abs(fmin) + tol.  if   f   is not unimodal,
-    // then fmin may approximate a local, but perhaps non-global, minimum to
-    // the same accuracy.
-    //     this function subprogram is a slightly modified  version  of  the
-    // algol  60 procedure  localmin  given in richard brent, algorithms for
-    // minimization without derivatives, prentice - hall, inc. (1973).
-    //
-    //
-    if (!isFinite(ax) || !isFinite(bx))
-        throw "Bounds ax, bx must be finite: " + ax + ", " + bx;
-    var a, b, c, d, e, eps, xm, p, q, r, tol1, tol2, u, v, w, fu, fv, fw, fx, x;
-    var i = 0;
-    function sign(x, y) {
-        // return value of x with the sign of y.
-        return y >= 0 ? Math.abs(x) : -Math.abs(x);
-    }
-    //
-    //  c is the squared inverse of the golden ratio
-    //
+        //var ax,bx,f,tol;
+        //
+        //     an approximation  x  to the point where  f  attains a minimum  on
+        // the interval  (ax,bx)  is determined.
+        //
+        //
+        // input..
+        //
+        // ax    left endpoint of initial interval
+        // bx    right endpoint of initial interval
+        // f     function subprogram which evaluates  f(x)  for any  x
+        //       in the interval  (ax,bx)
+        // tol   desired length of the interval of uncertainty of the final
+        //       result ( >= 0.0)
+        //
+        //
+        // output..
+        //
+        // fmin  abcissa approximating the point where  f  attains a minimum
+        //
+        //
+        //     the method used is a combination of  golden  section  search  and
+        // successive parabolic interpolation.  convergence is never much slower
+        // than  that  for  a  fibonacci search.  if  f  has a continuous second
+        // derivative which is positive at the minimum (which is not  at  ax  or
+        // bx),  then  convergence  is  superlinear, and usually of the order of
+        // about  1.324....
+        //     the function  f  is never evaluated at two points closer together
+        // than  eps*Math.abs(fmin) + (tol/3), where eps is  approximately the square
+        // root  of  the  relative  machine  precision.   if   f   is a unimodal
+        // function and the computed values of   f   are  always  unimodal  when
+        // separated by at least  eps*Math.abs(x) + (tol/3), then  fmin  approximates
+        // the abcissa of the global minimum of  f  on the interval  ax,bx  with
+        // an error less than  3*eps*Math.abs(fmin) + tol.  if   f   is not unimodal,
+        // then fmin may approximate a local, but perhaps non-global, minimum to
+        // the same accuracy.
+        //     this function subprogram is a slightly modified  version  of  the
+        // algol  60 procedure  localmin  given in richard brent, algorithms for
+        // minimization without derivatives, prentice - hall, inc. (1973).
+        //
+        //
+    if(!isFinite(ax) || !isFinite(bx))
+      throw `Bounds ax, bx must be finite: ${ax}, ${bx}`    
+        
+  var a: number,
+    b: number,
+    c: number,
+    d: number,
+    e: number,
+    eps: number,
+    xm: number,
+    p: number,
+    q: number,
+    r: number,
+    tol1: number,
+    tol2: number,
+    u: number,
+    v: number,
+    w: number,
+    fu: number,
+    fv: number,
+    fw: number,
+    fx: number,
+    x : number;
+    
+  var i: number = 0;
+  
+
+    function sign(x: number, y: number): number {
+            // return value of x with the sign of y.
+            return y >= 0 ? Math.abs(x) : -Math.abs(x);
+        }
+        //
+        //  c is the squared inverse of the golden ratio
+        //
     c = 0.5 * (3.0 - Math.sqrt(5.0));
     //
     //  eps is approximately the square root of the relative machine
@@ -83,7 +112,9 @@ function fmin(ax, bx, f, tol) {
     //  main loop starts here
     //
     for (;;) {
+        
         i++;
+        
         xm = 0.5 * (a + b);
         tol1 = eps * Math.abs(x) + tol / 3.0;
         tol2 = 2.0 * tol1;
@@ -128,8 +159,7 @@ function fmin(ax, bx, f, tol) {
                 if ((b - u) < tol2) {
                     d = sign(tol1, xm - x);
                 }
-            }
-            else {
+            } else {
                 //
                 //  a golden-section step
                 //
@@ -141,8 +171,7 @@ function fmin(ax, bx, f, tol) {
                 }
                 d = c * e;
             }
-        }
-        else {
+        } else {
             //
             //  a golden-section step
             //
@@ -199,8 +228,11 @@ function fmin(ax, bx, f, tol) {
             fv = fu;
         }
     }
+
     return { x: x, fx: fx, i: i };
 }
+
 module.exports = {
-    fmin: fmin
-};
+  fmin: fmin
+}
+
